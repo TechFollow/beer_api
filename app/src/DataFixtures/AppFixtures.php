12d15d3eaa8b90e3a56e60197015c77e39040bff
RACAUD Simon <simonracaud@gmail.com>
2020-05-29 11:34:43 +0200

    Feat(DataFixture: AppFixture) Create more user + Create Checkins + Reset Tables id

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -3,8 +3,9 @@
 namespace App\DataFixtures;
 
 use App\Entity\Beer;
-use App\Entity\Brasserie;
 use App\Entity\User;
+use App\Entity\Checkin;
+use App\Entity\Brasserie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -16,15 +17,29 @@ class AppFixtures extends Fixture
         $line = 0;
         $data = [];
 
+        $this->prepare($manager);
+
+        $user1 = $this->create_user("root", "test@email.fr", "root");
+        $user2 = $this->create_user("userX", "abc@email.fr", "user");
+        $manager->persist($user1);
+        $manager->persist($user2);
+
         if (($file = fopen("../open-beer-database.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($file, 1200, ";")) !== FALSE) {
                 if ($data == NULL || $data[0] == NULL || count($data) != 22) {
                     continue;
                 }
+                if (($line % 2) == 0) {
+                    $user = $user1;
+                } else {
+                    $user = $user2;
+                }
                 $brasserie = $this->create_brasserie($data);
                 $manager->persist($brasserie);
                 $beer = $this->create_beer($data, $brasserie);
                 $manager->persist($beer);
+                $checkin = $this->create_checkin($user, $beer);
+                $manager->persist($checkin);
                 $line++;
                 if ($line >= $max_line) {
                     break;
@@ -32,18 +47,39 @@ class AppFixtures extends Fixture
             }
         }
         fclose($file);
-        $user = $this->create_user();
-        $manager->persist($user);
         $manager->flush();
     }
 
-    private function create_user() : User
+    private function prepare(ObjectManager $manager)
+    {
+        $conn = $manager->getConnection();
+
+        $conn->exec("ALTER TABLE beer AUTO_INCREMENT = 1;");
+        $conn->exec("ALTER TABLE user AUTO_INCREMENT = 1;");
+        $conn->exec("ALTER TABLE brasserie AUTO_INCREMENT = 1;");
+        $conn->exec("ALTER TABLE checkin AUTO_INCREMENT = 1;");
+    }
+
+    private function create_checkin(User $user, Beer $beer) : Checkin
+    {
+        $checkin = new Checkin();
+        $mark = (rand(0, 100) / 10);
+
+        $checkin->setMark($mark);
+        $checkin->setBeer($beer);
+        $checkin->setUser($user);
+        $checkin->setDateCreate(new \DateTime());
+        $checkin->setDateUpdate(new \DateTime());
+        return $checkin;
+    }
+
+    private function create_user($pseudo, $email, $passwd) : User
     {
         $user = new User();
-        $user->setEmail("test@email.fr");
-        $user->setPassword("root");
-        $user->setRole(['admin']);
-        $user->setPseudo("root");
+        $user->setEmail($email);
+        $user->setPassword($passwd);
+        $user->setRole(['role']);
+        $user->setPseudo($pseudo);
         $user->setDateCreate(new \DateTime());
         $user->setDateUpdate(new \DateTime());
         return $user;
